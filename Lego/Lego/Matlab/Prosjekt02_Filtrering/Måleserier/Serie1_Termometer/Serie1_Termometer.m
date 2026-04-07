2ed897af62b139850899688f205a06f651add838
Belmin-a-davis <began7394@uis.no>
2026-04-07 11:30:55 +0200

    i wish i were levar burton! LIGHT SPEED DONT RAINBOW READ ME NYIGGUH

--- Lego/Lego/Matlab/Prosjekt02_Filtrering/Måleserier/Serie1_Termometer/Serie1_Termometer.m
+++ Lego/Lego/Matlab/Prosjekt02_Filtrering/Måleserier/Serie1_Termometer/Serie1_Termometer.m
@@ -1,5 +1,10 @@
 % Måling av temperatur med termometer
 clear; close all
-load('Serie1_Sprang.mat')
-T = Lys;  % temperatur i det kalde og varme vannet
+load('filnavn.mat')
+T = Lys; % temperatur i det kalde og varme vannet
 
+% IIR low-pass filter
+tau = 3.4;
+Ts = 0.12;
+tid = (0:length(T)-1) * Ts;
+LavpassFilter(tid, T, tau=tau);
